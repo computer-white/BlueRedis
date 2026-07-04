a2ccdd43280c5e6c677d461b386987f92e43f4e6
blue <homeheyang@outlook.com>
2026-07-04 12:29:01 +0800

    blue 对于blue.h的完善

--- blue/blue.h
+++ blue/blue.h
@@ -1,3 +1,11 @@
+/**
+ * @file blue.h
+ * @brief 一些头文件
+ * @author blue
+ * @email homeheyang@outlook.com
+ * @date 2026.4.5
+ * @copyright Copyright (c) 2026年 blue
+ */
 #ifndef BLUE_BLUE_H
 #define BLUE_BLUE_H
 #include "config.h"
